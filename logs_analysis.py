b44f37eb6cb79cf66b1a646b15c3df97b151a32c
Chiyoka Ide <chiyoka_66@hotmail.com>
2017-09-02 23:42:32 +0200

    add PopularArticle function

--- logs_analysis.py
+++ logs_analysis.py
@@ -3,10 +3,21 @@
 
 import psycopg2
 """ Connect to the PostgreSQL database. Returns a database connection. """
-def connect(database_name="newsdata"):
+def connect(database_name="news"):
 	try:
 		db = psycopg2.connect("dbname={}".format(database_name))
-		cursor = db.cursor
+		cursor = db.cursor()
 		return db, cursor
 	except:
 		print("Error establishing a database connection")
+
+def PopularArticle():
+	""" Print out the most popular article of all times. """
+	db, cursor = connect()
+	cursor.execute("SELECT path, COUNT(id) AS view FROM log GROUP BY path ORDER BY view DESC LIMIT 10;")
+	rows = cursor.fetchall()
+	db.close();
+	return rows
+
+	# select path, count(id) as view from log group by path order by view desc limit 10;
+PopularArticle()
