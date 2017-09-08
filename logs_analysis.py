fdc73f097db7f8017791a35e2a7ba89d8536ee52
Chiyoka Ide <chiyoka_66@hotmail.com>
2017-09-08 23:40:59 +0200

    add a experiment as comment out lines

--- logs_analysis.py
+++ logs_analysis.py
@@ -23,14 +23,18 @@ def Popular3Article():
 		ls = re.sub('/article/', '', ls)
 		ls = "'" + ls + "'"
 		query = ("SELECT title from articles where slug = " + str(ls))
-		# get the title from articles table
+		#get the title from articles table
 		cursor.execute(query)
 		title = cursor.fetchone()
 		title = title + (r[1],)  # add a view count to title tuple
 		row.append(title)
+	# 	row.append(ls)
+	# cursor.execute("SELECT articles.title, count(log.path) AS view FROM articles, log WHERE articles.slug = " + str(row[0]) + "OR articles.slug = " + str(row[1]) + "OR articles.slug = " + str(row[2]) + "GROUP BY articles.title ORDER BY view desc")
+	# top3 = cursor.fetchall()
 
 	db.close();
 	return row
+
 
 print Popular3Article()
 
