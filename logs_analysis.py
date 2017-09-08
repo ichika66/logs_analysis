621e6fd83ed3363c856ceb9d20320b532ab06100
Chiyoka Ide <chiyoka_66@hotmail.com>
2017-09-08 22:41:10 +0200

    def PopularArticle to handle two sql

--- logs_analysis.py
+++ logs_analysis.py
@@ -16,30 +16,57 @@ def PopularArticle():
 	db, cursor = connect()
 	cursor.execute("SELECT path, count(id) AS view FROM log WHERE path LIKE '/article/%' GROUP BY path ORDER BY view DESC LIMIT 3;")
 	rows = cursor.fetchall()
-	db.close();
+	#db.close();
 	row = []
 	ls = []
+	title = ''
+	dictionary = dict(rows)
+	#for r in rows:
 	for r in rows:
-		ls = r[0]  # get one element from tuple
-		ls = re.sub('/article/', '', ls)  # delete article from path
-		row.append(ls)  # append the row list
-	return GetTitle(row)
-
-def GetTitle(row):
-	""" get title from articles table which path is same as slug """
-	title = []  # list to store titles
-	db, cursor = connect()
-	cursor.execute("SELECT slug, title FROM articles;")
-	title = cursor.fetchall()
+		#ls = r[0]  # get one element from tuple
+		ls = r[0]
+		ls = re.sub('/article/', '', ls)
+		#newls = re.sub('/article/', '', ls)  # delete article from path
+		#dictionary[newls] = dictionary.pop(ls)
+		#row.append(ls)  # append the row list
+		ls = "'" + ls + "'"
+		#print ls
+		query = ("SELECT title from articles where slug = " + str(ls))
+		# get the title from articles table
+		cursor.execute(query)
+		title = cursor.fetchone()
+		#print title
+		title = title + (r[1],)  # add a view count to title tuple
+		#print r[1]
+		#title = title + r[1]
+		row.append(title)
+		#r[0] = title
+		#row.append(row)
+	#dictionary = dict(rows)
+	#print dictionary
 	db.close();
+	return row
+	#return GetTitle(row)
 
-	dictionary = dict(title)  # change tuple to dictionary
-	top3 = []  # list of top three titles
-	for r in row:
-		if dictionary[r]:
-			top3.append(dictionary[r])
+# def GetTitle(row):
+# 	""" get title from articles table which path is same as slug """
+# 	title = []  # list to store titles
+# 	db, cursor = connect()
+# 	cursor.execute("SELECT slug, title FROM articles;")
+# 	title = cursor.fetchall()
+# 	db.close();
 
-	return top3
+# 	dictionary = dict(title)  # change tuple to dictionary
+# 	top3 = []  # list of top three titles
+# 	for r in row:
+# 		if dictionary[r]:
+# 			top3.append(dictionary[r])
+
+# 	return top3
+
+# def Top3Articles():
+# 	""" returns the top 3 popular articl from the database """
+
 
 print PopularArticle()
 
