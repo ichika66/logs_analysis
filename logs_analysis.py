ccde327f878ba8b260cc1257fd930e7d21678629
Chiyoka Ide <chiyoka_66@hotmail.com>
2017-10-14 19:27:59 +0200

    separate query from the function

--- logs_analysis.py
+++ logs_analysis.py
@@ -3,6 +3,15 @@
 import re
 import psycopg2
 """ Connect to the PostgreSQL database. Returns a database connection. """
+
+query1 = (
+	"select articles.title, count(*) as view "
+	"from articles inner join log on log.path "
+	"like concat ('%', articles.slug, '%') "
+	"group by articles.title, log.path "
+	"order by views desc limit 3;"
+	)
+
 def connect(database_name="news"):
 	try:
 		db = psycopg2.connect("dbname={}".format(database_name))
@@ -10,6 +19,15 @@ def connect(database_name="news"):
 		return db, cursor
 	except:
 		print("Error establishing a database connection")
+
+def GetResult(query):
+	"""Get the query result from the DB"""
+	db, cursor = connect()
+	cursor.execute(query)
+	titles = cursor.fetchall
+	db.close
+	return titles
+
 
 def Popular3Article():
 	""" Print out the most popular article of all times. """
@@ -37,7 +55,9 @@ def Popular3Article():
 
 def PopularAuthor():
 	db, cursor = connect()
-	query = ("select articles.title, authors.name from articles, authors where articles.author = authors.id")
+
+	query = ("select path, count(path) as view from log where path like '/article/%'  group by path order by view desc limit 8;")
+	#query = ("select articles.title, authors.name from articles, authors where articles.author = authors.id")
 	cursor.execute(query)
 	author = cursor.fetchall()
 	db.close()
@@ -45,3 +65,4 @@ def PopularAuthor():
 
 print Popular3Article()
 print PopularAuthor()
+print GetResult(query1)
