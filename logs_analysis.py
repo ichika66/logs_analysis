60f4872996d1e2ee962737d4c8f62742de403bb0
Chiyoka Ide <chiyoka_66@hotmail.com>
2017-10-14 19:46:00 +0200

    comment out old lines of functions

--- logs_analysis.py
+++ logs_analysis.py
@@ -29,40 +29,40 @@ def GetResult(query):
 	return titles
 
 
-def Popular3Article():
-	""" Print out the most popular article of all times. """
-	db, cursor = connect()
-	cursor.execute("SELECT path, count(id) AS view FROM log WHERE path LIKE '/article/%' GROUP BY path ORDER BY view DESC LIMIT 3;")
-	rows = cursor.fetchall()
-	row = []
-	ls = []
-	for r in rows:
-		ls = r[0]
-		ls = re.sub('/article/', '', ls)
-		ls = "'" + ls + "'"
-		query = ("SELECT title from articles where slug = " + str(ls))
-		#get the title from articles table
-		cursor.execute(query)
-		title = cursor.fetchone()
-		title = title + (r[1],)  # add a view count to title tuple
-		row.append(title)
-	# 	row.append(ls)
-	# cursor.execute("SELECT articles.title, count(log.path) AS view FROM articles, log WHERE articles.slug = " + str(row[0]) + "OR articles.slug = " + str(row[1]) + "OR articles.slug = " + str(row[2]) + "GROUP BY articles.title ORDER BY view desc")
-	# top3 = cursor.fetchall()
-	db.close();
-	return row
+# def Popular3Article():
+# 	""" Print out the most popular article of all times. """
+# 	db, cursor = connect()
+# 	cursor.execute("SELECT path, count(id) AS view FROM log WHERE path LIKE '/article/%' GROUP BY path ORDER BY view DESC LIMIT 3;")
+# 	rows = cursor.fetchall()
+# 	row = []
+# 	ls = []
+# 	for r in rows:
+# 		ls = r[0]
+# 		ls = re.sub('/article/', '', ls)
+# 		ls = "'" + ls + "'"
+# 		query = ("SELECT title from articles where slug = " + str(ls))
+# 		#get the title from articles table
+# 		cursor.execute(query)
+# 		title = cursor.fetchone()
+# 		title = title + (r[1],)  # add a view count to title tuple
+# 		row.append(title)
+# 	# 	row.append(ls)
+# 	# cursor.execute("SELECT articles.title, count(log.path) AS view FROM articles, log WHERE articles.slug = " + str(row[0]) + "OR articles.slug = " + str(row[1]) + "OR articles.slug = " + str(row[2]) + "GROUP BY articles.title ORDER BY view desc")
+# 	# top3 = cursor.fetchall()
+# 	db.close();
+# 	return row
 
 
-def PopularAuthor():
-	db, cursor = connect()
+# def PopularAuthor():
+# 	db, cursor = connect()
 
-	query = ("select path, count(path) as view from log where path like '/article/%'  group by path order by view desc limit 8;")
-	#query = ("select articles.title, authors.name from articles, authors where articles.author = authors.id")
-	cursor.execute(query)
-	author = cursor.fetchall()
-	db.close()
-	return author
+# 	query = ("select path, count(path) as view from log where path like '/article/%'  group by path order by view desc limit 8;")
+# 	#query = ("select articles.title, authors.name from articles, authors where articles.author = authors.id")
+# 	cursor.execute(query)
+# 	author = cursor.fetchall()
+# 	db.close()
+# 	return author
 
-print Popular3Article()
-print PopularAuthor()
+#print Popular3Article()
+#print PopularAuthor()
 print GetResult(query1)
